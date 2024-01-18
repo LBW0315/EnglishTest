dddc80147208d101635b046174de1a73c1c5318b
LBW315 <big.west.win.sentence.14@gmail.com>
2024-01-19 01:14:10 +0900

    教材の9章を参考にしながらリポジトリに部分一致のメソッドを追加。コントローラにunitという値に対しての部分一致のメソッドを追加

--- src/main/java/com/example/englishtest/controller/HomeController.java
+++ src/main/java/com/example/englishtest/controller/HomeController.java
@@ -1,13 +1,40 @@
 package com.example.englishtest.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.englishtest.entity.Word;
+import com.example.englishtest.repository.WordRepository;
 
 @Controller
 public class HomeController {
+	private final WordRepository wordRepository;
+	
+	public HomeController(WordRepository wordRepository) {
+		this.wordRepository = wordRepository;
+	}
+	
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
+	
+@GetMapping
+public String index2(Model model, @RequestParam(name = "unit", required = false)String unit) {
+	List<Word> wordPage;
+	
+	if (unit != null && !unit.isEmpty()) {
+        wordPage = wordRepository.findByNameLike("%" + unit + "%");                
+    } else {
+        wordPage = wordRepository.findAll();
+    } 
+	model.addAttribute("unit", unit);
+	
+	return "index";
+}
 
 }
--- src/main/java/com/example/englishtest/repository/WordRepository.java
+++ src/main/java/com/example/englishtest/repository/WordRepository.java
@@ -1,9 +1,13 @@
 package com.example.englishtest.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.englishtest.entity.Word;
 
 public interface WordRepository extends JpaRepository<Word, Integer> {
+	public List<Word> findByNameLike(String unit);
 
 }
