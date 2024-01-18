b789cef6382ab51f150e1ed20e1aef1ecb08e508
LBW315 <big.west.win.sentence.14@gmail.com>
2024-01-19 02:00:37 +0900

    同じパスにハンドラメソッドがマッピングされていることが原因でエラーがでた。パスの重複を避けるためにとりあえず変更

--- src/main/java/com/example/englishtest/controller/HomeController.java
+++ src/main/java/com/example/englishtest/controller/HomeController.java
@@ -13,28 +13,30 @@ import com.example.englishtest.repository.WordRepository;
 @Controller
 public class HomeController {
 	private final WordRepository wordRepository;
-	
+
 	public HomeController(WordRepository wordRepository) {
 		this.wordRepository = wordRepository;
 	}
-	
+
+
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
 	
-@GetMapping("/")
-public String index2(Model model, @RequestParam(name = "unit", required = false)String unit) {
-	List<Word> wordPage;
-	
-	if (unit != null && !unit.isEmpty()) {
-        wordPage = wordRepository.findByUnitLike("%" + unit + "%");                
-    } else {
-        wordPage = wordRepository.findAll();
-    } 
-	model.addAttribute("unit", unit);
-	
-	return "index";
-}
+
+	@GetMapping("/another")
+	public String rangeSelection(Model model, @RequestParam(name = "unit", required = false) String unit) {
+		List<Word> wordPage;
+
+		if (unit != null && !unit.isEmpty()) {
+			wordPage = wordRepository.findByUnitLike("%" + unit + "%");
+		} else {
+			wordPage = wordRepository.findAll();
+		}
+		model.addAttribute("unit", unit);
+
+		return "index";
+	}
 
 }
