6792a2c78b7ad12054c0e5a0c05ca0a9e2697eb8
LBW315 <big.west.win.sentence.14@gmail.com>
2024-01-20 09:44:15 +0900

    新しく追加した学年がintだったため数字に変換。リポジトリのJPAインターフェースを継承したロジックをSQLでいうところのANDのロジックに変更

--- src/main/java/com/example/englishtest/repository/WordRepository.java
+++ src/main/java/com/example/englishtest/repository/WordRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.englishtest.entity.Word;
 
 public interface WordRepository extends JpaRepository<Word, Integer> {
-	public List<Word> findByUnitLike(String unit);
+	public List<Word> findBySchoolyearAndUnit(int schoolyear, String unit);
 
 }
