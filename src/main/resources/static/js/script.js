6bd69c98f9f9d3bafcdd1b095f6f25c2215fc565
LBW315 <big.west.win.sentence.14@gmail.com>
2024-01-21 11:37:34 +0900

    学年と範囲を選択してボタンを押すと条件にあった単語を取得することに成功！

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -21,94 +21,94 @@ const count = document.getElementById("count");
 
 
 const createText = () => {
-  // 正タイプした文字列をクリア
-  //スコアのインクリメント
-  score++;
-  typed = "";
-  typedfield.textContent = typed;
+	// 正タイプした文字列をクリア
+	//スコアのインクリメント
+	score++;
+	typed = "";
+	typedfield.textContent = typed;
 
-  // 配列のインデックス数からランダムな数値を生成する
-  let random = Math.floor(Math.random() * wordPage.length);
+	// 配列のインデックス数からランダムな数値を生成する
+	let random = Math.floor(Math.random() * wordPage.length);
 
-  untyped = wordPage[random].mean;
-  untypedfield.textContent = untyped;
+	untyped = wordPage[random].mean;
+	untypedfield.textContent = untyped;
 };
 
 // キー入力の判定
 const keyPress = (e) => {
-  // 誤タイプの場合
-  if (e.key !== untyped.substring(0, 1)) {
-    wrap.classList.add("mistyped");
-    setTimeout(() => {
-      wrap.classList.remove("mistyped");
-    }, 200);
-    return;
-  }
-  //正タイプの場合
-  wrap.classList.remove("mistyped");
-  typed += untyped.substring(0, 1);
-  untyped = untyped.substring(1);
-  typedfield.textContent = typed;
-  untypedfield.textContent = untyped;
+	// 誤タイプの場合
+	if (e.key !== untyped.substring(0, 1)) {
+		wrap.classList.add("mistyped");
+		setTimeout(() => {
+			wrap.classList.remove("mistyped");
+		}, 200);
+		return;
+	}
+	//正タイプの場合
+	wrap.classList.remove("mistyped");
+	typed += untyped.substring(0, 1);
+	untyped = untyped.substring(1);
+	typedfield.textContent = typed;
+	untypedfield.textContent = untyped;
 
-  //入力確認
-  console.log(e.key);
-  console.log(typed);
-  console.log(untyped);
+	//入力確認
+	console.log(e.key);
+	console.log(typed);
+	console.log(untyped);
 
-  // テキストがなくなったら新しいテキストを表示
-  if (untyped === "") {
-    createText();
-  }
+	// テキストがなくなったら新しいテキストを表示
+	if (untyped === "") {
+		createText();
+	}
 };
 
 // タイピングスキルのランクを判定
 const rankCheck = (score) => {
-  // テストの結果を返す
-  return `${score}文字打てました\n【OK】ボタンでもう一度チャレンジ`;
+	// テストの結果を返す
+	return `${score}文字打てました\n【OK】ボタンでもう一度チャレンジ`;
 };
 
 // ゲームを終了
 const gameOver = (id) => {
-  clearInterval(id);
+	clearInterval(id);
 
-  const result = confirm(rankCheck(score));
+	const result = confirm(rankCheck(score));
 
-  //OKボタンをクリックされたらリロードする
-  window.location.reload();
+	//OKボタンをクリックされたらリロードする
+	window.location.reload();
 };
 
 // カウントダウンタイマー
 const timer = () => {
-  // タイマー部分のHTML要素（p要素）を取得する
-  let time = count.textContent;
+	// タイマー部分のHTML要素（p要素）を取得する
+	let time = count.textContent;
 
-  const id = setInterval(() => {
-    //カウントダウンをする
-    time--;
-    count.textContent = time;
+	const id = setInterval(() => {
+		//カウントダウンをする
+		time--;
+		count.textContent = time;
 
-    // カウントが0になったらタイマーを停止する
-    if (time <= 0) {
-      clearInterval(id);
-      gameOver(id);
-    }
-  }, 1000);
+		// カウントが0になったらタイマーを停止する
+		if (time <= 0) {
+			clearInterval(id);
+			gameOver(id);
+		}
+	}, 1000);
 };
 
 //スタートボタンを押してゲームをスタートした後の処理
 start.addEventListener("click", () => {
-  //ランダムなテキストを表示する
-  createText();
+	//ランダムなテキストを表示する
+	createText();
 
-  //カウントダウンタイマーを開始する
-  timer();
+	//カウントダウンタイマーを開始する
+	timer();
 
-  //スタートボタンを非表示にする
-  start.style.display = "none";
+	//スタートボタンを非表示にする
+	start.style.display = "none";
 
-  //キーボードのイベント処理(キーボード押したらkeyPressメソッドを開始しますよ)
-  document.addEventListener("keypress", keyPress);
+	//キーボードのイベント処理(キーボード押したらkeyPressメソッドを開始しますよ)
+	document.addEventListener("keypress", keyPress);
 });
 
 //スタートボタンを押していない時のテキスト
