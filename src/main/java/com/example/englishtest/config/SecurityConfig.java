0540c89ef55c73c92ec2b1c8336991cd83f0f373
LBW315 <big.west.win.sentence.14@gmail.com>
2024-01-24 18:07:23 +0900

    誰でも使えるように設定

---
+++ src/main/java/com/example/englishtest/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.englishtest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(login -> login.requestMatchers("/**").permitAll());
+		return http.build();
+	}
+}
