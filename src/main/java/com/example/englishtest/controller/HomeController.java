3bb22261a2639b51755330e3ff9106c1ac370dc3
LBW315 <big.west.win.sentence.14@gmail.com>
2024-01-29 10:20:50 +0900

    [update] rangeSelectionハンドラにcountselect(時間設定)のデータを渡すことでリロードしても選んだ勉強時間が表示され続けるように実装

--- src/main/java/com/example/englishtest/controller/HomeController.java
+++ src/main/java/com/example/englishtest/controller/HomeController.java
@@ -18,16 +18,15 @@ public class HomeController {
 		this.wordRepository = wordRepository;
 	}
 
-	
-		@GetMapping("/")
-		public String index() {
-			return "index";
-		}
-		
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 
 	@GetMapping("/rangeselect")
 	public String rangeSelection(Model model, @RequestParam(name = "schoolyear", required = false) Integer schoolyear,
-			@RequestParam(name = "unit", required = false) String unit) {
+			@RequestParam(name = "unit", required = false) String unit,
+			@RequestParam(name = "countselect", required = false) String countselect) {
 		List<Word> wordPage;
 
 		//unitとschoolyearの両方をif文の条件に入れるには？
@@ -41,6 +40,7 @@ public class HomeController {
 		}
 		model.addAttribute("unit", unit);
 		model.addAttribute("schoolyear", schoolyear);
+		model.addAttribute("countselect", countselect);
 
 		model.addAttribute("wordPage", wordPage);
 		System.out.println(wordPage);
